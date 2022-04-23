13f226869563fd0c5896e55097c5541431e86b8d
zgs3 <zkairaitis@gmail.com>
2022-04-23 16:53:46 +0300

    Added main HTML structure with CSS styling.

---
+++ script/script.js
@@ -0,0 +1,6 @@
+document.querySelector("#date").innerHTML= new Date().toDateString();
+
+const mainInput = document.querySelector('.mainInput');
+const submitBtn = document.querySelector('.submitBtn');
+const itemsUl = document.querySelector('.itemsUl');
+
