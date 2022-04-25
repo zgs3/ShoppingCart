9aabf009053cd29cb10644b0cfe8e67fde674928
zgs3 <zkairaitis@gmail.com>
2022-04-25 15:07:09 +0300

    Added first part of JS for creating new list element. Updated HTML

--- script/script.js
+++ script/script.js
@@ -4,3 +4,15 @@ const mainInput = document.querySelector('.mainInput');
 const submitBtn = document.querySelector('.submitBtn');
 const itemsUl = document.querySelector('.itemsUl');
 
+submitBtn.addEventListener('click', itemAdder);
+
+function itemAdder(event){
+  event.preventDefault();
+  const itemDiv = document.createElement('div');
+  itemDiv.classList.add('item');
+  const newItemLi = document.createElement('li');
+  newItemLi.innerText = 'test';
+  newItemLi.classList.add('itemLi')
+  itemDiv.append(newItemLi);
+  itemsUl.append(itemDiv);
+}
