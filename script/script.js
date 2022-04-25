722f0a0f46eb3b3940b5778b325692524d56bc17
zgs3 <zkairaitis@gmail.com>
2022-04-25 15:37:16 +0300

    Updated JS item add function. Updated CSS.

--- script/script.js
+++ script/script.js
@@ -10,9 +10,22 @@ function itemAdder(event){
   event.preventDefault();
   const itemDiv = document.createElement('div');
   itemDiv.classList.add('item');
+  itemDiv.classList.add('flex');
   const newItemLi = document.createElement('li');
-  newItemLi.innerText = 'test';
+  newItemLi.innerText = mainInput.value;
+  mainInput.value = '';
   newItemLi.classList.add('itemLi')
   itemDiv.append(newItemLi);
+  // adding check button
+  const checkBtn = document.createElement('button');
+  checkBtn.innerHTML = '<i class="fas fa-circle-check"></i>';
+  checkBtn.classList.add('checkBtn');
+  itemDiv.append(checkBtn);
+  // adding delete button
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = '<i class="fas fa-trash-can"></i>';
+  deleteBtn.classList.add('deleteBtn');
+  itemDiv.append(deleteBtn);
+  // appending whole new div to ul
   itemsUl.append(itemDiv);
 }
